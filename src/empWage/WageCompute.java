57de827fe488b1fffeb0eff97cb0b0c25f36a5ab
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-09-25 23:05:02 +0530

    Initial commit

--- src/empWage/WageCompute.java
+++ src/empWage/WageCompute.java
@@ -6,29 +6,36 @@ public class WageCompute {
 		int IS_FULL_TIME = 2;
 		int IS_PART_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
+		int NUM_OF_WORKING_DAYS=20;
+		int totalEmpWage = 0;
 		
 		int empHrs = 0;
 		int empWage = 0;
-		
-		int empCheck = (int) Math.floor(Math.random()*10)%2;
-		switch(empCheck) 
+		for(int day = 0; day < NUM_OF_WORKING_DAYS; day++ )
 		{
-		case 1: 
-			empHrs = 8;
-			System.out.print("Employee Present Fulltime ");
-			break;
-			
-		case 2:
-			empHrs = 4;
-			System.out.print("Employee Present Parttime ");
-			break;
-		default:
-			empHrs = 0;	
-			System.out.print("Employee absent ");
-		}	
+			int empCheck = (int) Math.floor(Math.random()*10)%2;
+			switch(empCheck) 
+			{
+			case 1: 
+				empHrs = 8;
+				System.out.print("Employee Present Fulltime ");
+				break;
+				
+			case 2:
+				empHrs = 4;
+				System.out.print("Employee Present Parttime ");
+				break;
+			default:
+				empHrs = 0;	
+				System.out.print("Employee absent ");
+			}	
 			    empWage = empHrs * EMP_RATE_PER_HOUR;
 			    System.out.println("Emp Wage: " + empWage);
-			    
+			    totalEmpWage += empWage ;
+	}
+		
+		System.out.println("Total Emp Wage: " + totalEmpWage);		
+
 
 
 
