8a40573b547ead0682232adc752e7a616175a198
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-09-25 23:07:07 +0530

    Initial commit

--- src/empWage/WageCompute.java
+++ src/empWage/WageCompute.java
@@ -3,38 +3,39 @@ package empWage;
 public class WageCompute {
 	public static void main(String[] args) {
 		
-		int IS_FULL_TIME = 2;
 		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-		int NUM_OF_WORKING_DAYS=20;
-		int totalEmpWage = 0;
-		
 		int empHrs = 0;
 		int empWage = 0;
-		for(int day = 0; day < NUM_OF_WORKING_DAYS; day++ )
-		{
-			int empCheck = (int) Math.floor(Math.random()*10)%2;
-			switch(empCheck) 
-			{
-			case 1: 
-				empHrs = 8;
-				System.out.print("Employee Present Fulltime ");
-				break;
-				
-			case 2:
-				empHrs = 4;
-				System.out.print("Employee Present Parttime ");
-				break;
-			default:
-				empHrs = 0;	
-				System.out.print("Employee absent ");
-			}	
-			    empWage = empHrs * EMP_RATE_PER_HOUR;
-			    System.out.println("Emp Wage: " + empWage);
-			    totalEmpWage += empWage ;
-	}
-		
-		System.out.println("Total Emp Wage: " + totalEmpWage);		
+		int NUM_OF_WORKING_DAYS=20;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
+		int MAX_HRS_IN_MONTH = 100;
+		while(totalWorkingDays<=NUM_OF_WORKING_DAYS && totalEmpHrs<= MAX_HRS_IN_MONTH)
+		{	totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+				switch(empCheck) 
+				{
+				case 1: 
+					empHrs = 8;
+					System.out.print("Employee Present Fulltime ");
+					break;
+					
+				case 2:
+					empHrs = 4;
+					System.out.print("Employee Present Parttime ");
+					break;
+				default:
+					empHrs = 0;	
+					System.out.print("Employee absent ");
+				}	
+				totalEmpHrs += empHrs;
+				System.out.println("Day#: " +totalWorkingDays + "Emp Hr: " +empHrs);
+		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR ;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 
 
 
